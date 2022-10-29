8840fa25e8189a0c58ac82b7275d7a2626e41ab7
DifenYedidya <difen.yedidya@gmail.com>
2022-10-29 19:36:49 +0700

    create data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Create from './components/create';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="main">
-      <h1 className='main-header'>crud react</h1>
-    </div>
+    <Router>
+      <div className="main">
+        <h1 className='main-header'>crud react</h1>
+        <div>
+          <Create/>
+        </div>
+      </div>
+    </Router>
   );
 }
 
