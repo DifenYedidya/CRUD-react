73a7ad2efa522b3acd81cfb9d759d9bde397f184
DifenYedidya <difen.yedidya@gmail.com>
2022-10-29 21:12:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Create from './components/create';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Read from './components/read';
+import Update from './components/update';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="main">
-        <h1 className='main-header'>crud react</h1>
-        <div>
-          <Create/>
+        <h2 className="main-header">React Crud Operations</h2>
+        <Routes>
+          <Route path='/create' element={<Create />} />
+          <Route path='/read' element={<Read />} />  
+          <Route path='/update' element={<Update />} />
+        </Routes>
+        <div className='link'>
+          <h2>Go to</h2>
+          <Link to='/create'><h3>create page</h3></Link>
+          <Link to='/read'><h3>read page</h3></Link>
         </div>
       </div>
     </Router>
